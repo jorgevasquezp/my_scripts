4c0907bdd97dc460ad8c32719c4a5d46935655e8
yorch <yorch@yorchnet.com>
2016-10-21 11:08:32 -0700

    added scene detection to the makine web compression setting

---
+++ bash/make_mkn_web.sh
@@ -0,0 +1,10 @@
+b=8M;
+min=16M;
+max=64M;
+g=6;
+
+for i;
+	do
+	ffmpeg -ss $slate -i "$i" \
+		-c:v libx264 -b:v $b -minrate $min -maxrate $max -vf yadif -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -strict -2 -movflags faststart -q 0 "${i%.*}_web.mp4"
+	done
