c20ebfea6c9623c93ede5134d9539a57691947d2
yorch <yorch@yorchnet.com>
2016-10-27 12:13:04 -0700

    Created a ffmpeg script to make a center cropped square version of a video, still under development

---
+++ bash/make_social.sh
@@ -0,0 +1,15 @@
+#crop to a centered square
+export square_crop="crop=in_h:in_h:(in_w-in_h)/2:0"
+
+b=8M;
+min=16M;
+max=64M;
+g=6;
+
+for i;
+	do
+	ffmpeg -ss $slate -i "$i" \
+		-c:v libx264 -b:v $b -minrate $min -maxrate $max -filter:v "$square_crop" -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -strict -2 -movflags faststart -aspect 1:1 -q 0 -s 1080x1080 "${i%.*}_instagram.mp4"
+		-c:v libx264 -b:v $b -minrate $min -maxrate $max -filter:v "$square_crop" -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -strict -2 -movflags faststart -aspect 1:1 -q 0 -s 1080x1080 "${i%.*}_facebook.mp4"
+		
+	done
