1e74181967821a8637fcf1dc66b1f1b77990cea7
yorch <yorch@yorchnet.com>
2016-10-27 12:10:33 -0700

    Added scene change detection options for keyframing in the h264 converter scripts

--- bash/make_h264+.sh
+++ bash/make_h264+.sh
@@ -1,4 +1,5 @@
+g=6;
 for i;
-	do ffmpeg -i "$i" -c:v libx264 -profile:v main -pix_fmt yuv420p -q 0 -strict -2 "${i%.*}_h264.mov" ;
+	do ffmpeg -i "$i" -c:v libx264 -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -q 0 -strict -2 "${i%.*}_h264.mov" ;
 done;
 	
--- bash/make_h264.sh
+++ bash/make_h264.sh
@@ -1,8 +1,5 @@
+g=6;
 for i;
-<<<<<<< HEAD
-	do ffmpeg -i "$i" -c:v libx264 -profile:v main -pix_fmt yuv420p -q 0 -strict -2 -vb 5000k "${i%.*}_h264.mov" ;
-=======
-	do ffmpeg -i "$i" -c:v libx264 -profile:v main -pix_fmt yuv420p -q 0 -strict -2 "${i%.*}_h264.mov" ;
->>>>>>> a4f1bdc7ca935a4d555d2c313b1317af6b687756
+	do ffmpeg -i "$i" -c:v libx264 -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -q 0 -strict -2 -vb 5000k "${i%.*}_h264.mov" ;
 done;
 	
