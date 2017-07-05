74d153a4ffc1dc64e4525bdf68cb095bbd0e13e3
Jorge Vásquez Pérez <yorch.vasquez@gmail.com>
2017-07-05 12:09:24 -0700

    got rid of the yadiff filter and declared the frames progressive instead

--- bash/make_mkn_web.sh
+++ bash/make_mkn_web.sh
@@ -6,5 +6,5 @@ g=6;
 for i;
 	do
 	ffmpeg -ss $slate -i "$i" \
-		-c:v libx264 -b:v $b -minrate $min -maxrate $max -vf yadif -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -strict -2 -movflags faststart -q 0 "${i%.*}_web.mp4"
+		-c:v libx264 -b:v $b -minrate $min -maxrate $max -vf "setfield=prog" -x264opts keyint=$g:scenecut=-1 -profile:v main -pix_fmt yuv420p -strict -2 -movflags faststart -q 0 "${i%.*}_web.mp4"
 	done
