8313faf0a597a04f15e4f13c7e01dadf6451a630
Jorge Vásquez Pérez <yorch.vasquez@gmail.com>
2017-07-27 15:05:05 -0700

    added a setting for makines wacom, and a ffmpeg script to make a qt out of a sequence overlayed over gray background

--- bash/fix_wacom.sh
+++ bash/fix_wacom.sh
@@ -1,3 +1,8 @@
-wacom_id=10
-xsetwacom --set $wacom_id button 2 3
-xsetwacom --set $wacom_id button 3 2
+#casa
+#wacom_id=10
+#makine
+wacom_id="Wacom Intuos Pro M Pen stylus"
+xsetwacom --set "$wacom_id" button 2 3
+xsetwacom --set "$wacom_id" button 3 2
+
+
--- bash/make_anim+.sh
+++ bash/make_anim+.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 bg_color=#5d5d5d
 size=1920x1080
-base=smoke1
 for i;
-do ffmpeg -f lavfi -i color=c=$bg_color:s=$size -i $base.%04d.png -filter_complex "[0:v][1:v]overlay=shortest=1,format=yuv420p[out]" -map "[out]" -c:v qtrle -q 0 $base"_Animation+.mov";
+do ffmpeg -f lavfi -i color=c=$bg_color:s=$size -start_number 0001 -i $i%04d.png -filter_complex "[0:v][1:v]overlay=shortest=1,format=yuv420p[out]" -map "[out]" -c:v qtrle -q 0 $i"_Animation+.mov";
 done
