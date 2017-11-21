56bb488cf4666e40bd7c4dbb1ee29eb6867ac05c
jorgevasquezp <yorch.vasquez@gmail.com>
2017-11-21 11:38:51 -0800

    The script that finds out the right device id for the wacom stylus, now trims the last tab in the output format

--- bash/fix_wacom.sh
+++ bash/fix_wacom.sh
@@ -2,7 +2,7 @@
 #wacom_id=10
 #makine
 #wacom_id="Wacom Intuos Pro M Pen stylus"
-wacom_id=$(xsetwacom --list devices | grep stylus | grep -o [0-9].)
+wacom_id=$(xsetwacom --list devices | grep stylus | grep -o [0-9]. | sed 's/\t//')
 xsetwacom --set "$wacom_id" button 2 3
 xsetwacom --set "$wacom_id" button 3 2
 
