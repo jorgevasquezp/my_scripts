b9827360457a00243c62e27e55be8053cc9cd5c0
Jorge Vásquez Pérez <yorch.vasquez@gmail.com>
2018-12-13 10:49:51 -0800

    Handle the paths in a way theat the resulting directory is relative to the last directory in the path instead of using the full path.

--- bash/ghostmirror.sh
+++ bash/ghostmirror.sh
@@ -1,1 +1,5 @@
-find "$1"/ -type d -exec mkdir -p "$2"/{} \; -o -type f -exec touch "$2"/{} \;;
+orig_dir=$(pwd)
+path=$(dirname $1)
+dirname=$(basename $1)
+cd $path;
+find "$dirname"/ -type d -exec mkdir -p "$2"/{} \; -o -type f -exec touch "$2"/{} \;;
