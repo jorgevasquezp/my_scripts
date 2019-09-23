92019ccecac6123066664374d3862e9fa3433c1d
jorgevasquezp <yorch.vasquez@gmail.com>
2019-09-23 13:04:41 -0700

    fixed the wacom script, to match new xsetwacom standards

--- bash/fix_wacom.sh
+++ bash/fix_wacom.sh
@@ -4,7 +4,5 @@
 #wacom_id="Wacom Intuos Pro M Pen stylus"
 #wacom_id=$(xsetwacom --list devices | grep stylus | grep -o [0-9]. | sed 's/\t//')
 wacom_id=$(xsetwacom --list devices | grep stylus | grep 'id\: [0-9].' -o | grep [0-9]. -o)
-xsetwacom --set "$wacom_id" button 2 3
-xsetwacom --set "$wacom_id" button 3 2
-
-
+xsetwacom --set "$(($wacom_id + 0))" button 2 3
+xsetwacom --set "$(($wacom_id + 0))" button 3 2
