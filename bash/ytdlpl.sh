35213a8f68c159f10f8f5df0b1bf289b09fb0dfd
jorgevasquezp <yorch.vasquez@gmail.com>
2020-01-23 15:18:36 -0800

    added more options to ytdlpl

--- bash/ytdlpl.sh
+++ bash/ytdlpl.sh
@@ -1,8 +1,26 @@
+format=bestvideo+bestaudio
+format=22
+while getopts ":a" opt; do
+  case $opt in
+    a)
+      format=bestaudio
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 export IFS=$'\n';
 for i;
 	do 
-		#youtube-dl -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -f bestaudio "$i"
-		youtube-dl -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -f bestvideo+bestaudio "$i"
+		#Audio-->
+		youtube-dl -x --audio-format vorbis --add-metadata --xattrs --embed-subs --embed-thumbnail --all-subs -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -i -f bestaudio --audio-quality 192k "$i"
+		#youtube-dl --xattrs --embed-subs --embed-thumbnail --all-subs -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -i -f bestaudio "$i"
+		#youtube-dl -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -f $a "$i"
+		#youtube-dl --add-metadata --xattrs --embed-subs --embed-thumbnail --all-subs -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -i -f bestvideo+bestaudio "$i"
+		#youtube-dl --add-metadata --xattrs --embed-subs --all-subs -o "%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s" -f bestvideo+bestaudio "$i"
+		#VIDEO-->
+		#youtube-dl --add-metadata --xattrs --embed-subs --all-subs -o "%(playlist)s/%(playlist_index)s - %(title)s - %(format)s.%(ext)s" -i -f $format "$i"
+		
 done;
-
-	
