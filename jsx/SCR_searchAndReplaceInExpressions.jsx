7701646ba868a4d54ce99014edb60592c16bda71
Jorge Vasquez <jorge@grfx12.intra.makinestudios.com>
2020-02-06 10:58:51 -0800

    Replaced line endings to Unix-like for consistency

---
+++ jsx/SCR_searchAndReplaceInExpressions.jsx
@@ -0,0 +1,18 @@
+﻿proj = app.project.activeItem;
+layers = proj.selectedLayers;
+search_string = 'comp("##DSDC036_Colors##").layer("Colors").effect(" ';
+replace_string = 'comp("##DSDC036_Colors##").layer("';
+
+
+for ( var i = 0; i < layers.length ; i ++ ){
+	var layer = layers[i];
+	var properties = layer.selectedProperties;
+	for ( var j = 0 ; j < layer.selectedProperties.length ; j ++ ){
+        var property = properties[j];
+        if ( property.canSetExpression ){
+		property.expression = property.expression.replace(search_string,replace_string);
+        }
+	}
+	
+}
+
--- jsx/dependencySearcher.jsx
+++ jsx/dependencySearcher.jsx
@@ -1,1 +1,33 @@
-﻿//Walk the items.search_string = "Long";project_items = app.project.itemsfor ( var i = 1; i <= project_items.length  ; i ++)    {        var item = project_items[i];        if( item.typeName == "Composition" )            {                var comp = item;                //alert( comp.name );                var comp = item;                for ( var j = 1 ; j <= comp.numLayers ; j ++ )                    {                        var layer = comp.layer(j);                        if ( layer.matchName == "ADBE AV Layer") //Only go into layers... not cameras and lights.                        {                            var effects = layer.property("Effects");                            //alert( comp.name + " , " + layer.name );                            for ( var k = 1 ; k <= effects.numProperties ; k ++ )                            {                                var effect = effects.property(k);                                if ( effect.name.search(search_string) != -1 )                                {                                    alert()                                    layer.comment = "Contains " + effect.name + " layer.";                                    comp.comment = "Contains " + effect.name + " layer.";                                }                                //alert( comp.name + " , " + layer.name + " , " + effect.name );                            }                        }                    }                                }    }
+﻿//Walk the items.
+search_string = "uni.";
+project_items = app.project.items
+for ( var i = 1; i <= project_items.length  ; i ++)
+    {
+        var item = project_items[i];
+        if( item.typeName == "Composition" )
+            {
+                var comp = item;
+                //alert( comp.name );
+                var comp = item;
+                for ( var j = 1 ; j <= comp.numLayers ; j ++ )
+                    {
+                        var layer = comp.layer(j);
+                        if ( layer.matchName == "ADBE AV Layer") //Only go into layers... not cameras and lights.
+                        {
+                            var effects = layer.property("Effects");
+                            //alert( comp.name + " , " + layer.name );
+                            for ( var k = 1 ; k <= effects.numProperties ; k ++ )
+                            {
+                                var effect = effects.property(k);
+                                if ( effect.name.search(search_string) != -1 )
+                                {
+                                    //alert()
+                                    layer.comment = "Contains " + effect.name + " layer.";
+                                    comp.comment = "Contains " + effect.name + " layer.";
+                                }
+                                //alert( comp.name + " , " + layer.name + " , " + effect.name );
+                            }
+                        }
+                    }                    
+            }
+    }
