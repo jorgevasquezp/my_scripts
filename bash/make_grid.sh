60228829b9f7ba3871b603eaacee6fe43772abbe
Jorge Vasquez <jorge@grfx12.intra.makinestudios.com>
2020-02-06 11:01:00 -0800

    added another option to deal with grid files

--- bash/make_grid.sh
+++ bash/make_grid.sh
@@ -7,7 +7,9 @@ filename=${@: -1}
 if [ -f $filename ]; then
    echo "Output $filename already exists."
 else
-   montage -label "\n%t" -pointsize 14 -tile 3x6 -geometry 550 -geometry +25+15 -border 1 $files $filename
+   #montage -label "\n%t" -pointsize 14 -tile 3x6 -geometry 550 -geometry +25+15 -border 1 $files $filename
+   #montage -background "#77A6E4" -label "\n%t" -pointsize 12 -tile 10x6 -geometry 120 -geometry +8+4 -border 1 $files $filename
+   montage -background "#77A6E4" -label "\n%t" -pointsize 12 -tile 10x6 -geometry 120 -geometry +8+4 -border 0 $files $filename
 fi
 
 
