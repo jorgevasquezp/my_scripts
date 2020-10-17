0bc7f2422d5199930f0abad60a706ad32d60a67a
Jorge Vasquez Perez <yorch@yorchnet.com>
2020-10-16 22:38:57 -0700

    The script to change the length of a comp and it's child layers, now also supports changing framerates. And added an expression to match the audio of a layer in a containing comp.

---
+++ jsx/match_audio_of_containing_comp.jsx
@@ -0,0 +1,4 @@
+//expresion to match the audio of the layer to an audio of a layer in a comp that contains this comp.
+my_comp = comp("COMPNAME");
+my_layer = my_comp.layer(thisComp.name);
+my_layer.inPoint + time
